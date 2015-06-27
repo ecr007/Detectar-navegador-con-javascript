e9b511ea9bf04838f8ac3d6bec64a851077016e9
Ever <evercuevas1000@gmail.com>
2015-06-27 10:18:52 -0400

    Update detect.js

--- detect.js
+++ detect.js
@@ -1,1 +1,19 @@
+// initializes main settings
+    var handleInit = function () {
 
+        if ($('body').css('direction') === 'rtl') {
+            isRTL = true;
+        }
+
+        isIE8 = !! navigator.userAgent.match(/MSIE 8.0/);
+        isIE9 = !! navigator.userAgent.match(/MSIE 9.0/);
+        isIE10 = !! navigator.userAgent.match(/MSIE 10.0/);
+
+        if (isIE10) {
+            jQuery('html').addClass('ie10'); // detect IE10 version
+        }
+        
+        if (isIE10 || isIE9 || isIE8) {
+            jQuery('html').addClass('ie'); // detect IE10 version
+        }
+    }
